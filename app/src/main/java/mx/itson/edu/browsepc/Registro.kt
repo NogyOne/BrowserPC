c0e8e6bde2d2f87cd56f3fd5b961565634486cd0
German <german_gomez1205@hotmail.com>
2023-05-08 00:29:20 -0700

    Normal register
    Nomal registar of the app

--- app/src/main/java/mx/itson/edu/browsepc/Registro.kt
+++ app/src/main/java/mx/itson/edu/browsepc/Registro.kt
@@ -1,13 +1,20 @@
 package mx.itson.edu.browsepc
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
+import org.w3c.dom.Text
 
 class Registro : AppCompatActivity() {
+    private val db = FirebaseFirestore.getInstance()
+    private val collection = db.collection("usuarios")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
@@ -16,6 +23,7 @@ class Registro : AppCompatActivity() {
         val userName: TextView = findViewById(R.id.txtUsername)
         val contrasena: TextView = findViewById(R.id.txtContrasena)
         val email: TextView = findViewById(R.id.txtEmail)
+        val celular: TextView = findViewById(R.id.txtCelular)
 
         registrar.setOnClickListener{
             when{
@@ -27,10 +35,17 @@ class Registro : AppCompatActivity() {
                userName.text.isEmpty()  || contrasena.text.isEmpty() || email.text.isEmpty() || !validarEmail(email.text.toString()) ->{
                    Toast.makeText(baseContext, "Todos los campos son obligatorios", Toast.LENGTH_SHORT).show()
 
-                }else->{
-                var intent: Intent = Intent(this, Bienvenida::class.java)
-                intent.putExtra("username", userName.text.toString())
-                startActivity(intent)
+                }else->{//Register
+                val user = Usuario(userName.text.toString(), email.text.toString(), contrasena.text.toString(), celular.text.toString())
+                collection.add(user).addOnSuccessListener { documentReference ->
+                    Log.d(TAG, "Usuario agregado con ID: ${documentReference.id}")
+                    var intent: Intent = Intent(this, Bienvenida::class.java)
+                    intent.putExtra("username", userName.text.toString())
+                    startActivity(intent)
+                }
+                    .addOnFailureListener{ e ->
+                        Log.w(TAG, "Error al registrar usuario", e)
+                    }
             }
             }
         }
--- app/src/main/java/mx/itson/edu/browsepc/Usuario.kt
+++ app/src/main/java/mx/itson/edu/browsepc/Usuario.kt
@@ -1,3 +1,6 @@
 package mx.itson.edu.browsepc
 
-data class Usuario(var id: String, var username: String, var email: String, var password: String, var celular: String)
+data class Usuario(var username: String,
+                   var email: String,
+                   var password: String,
+                   var celular: String)
